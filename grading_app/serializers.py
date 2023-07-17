23372df6ff8fae2e8ecbc2cd9b9a220aa5a7a44a
Faith Odunayo Adeosun <fadeosun@afexnigeria.com>
2023-07-17 14:10:57 +0100

    few changes made

--- grading_app/serializers.py
+++ grading_app/serializers.py
@@ -44,7 +44,7 @@ class GradeParameterSerializer(serializers.ModelSerializer):
 
 
 class CommodityGradeSerializer(serializers.ModelSerializer):
-    grade_parameter = GradeParameterSerializer()
+    grade_parameter = GradeParameterSerializer(many=True)
 
     class Meta:
         model = CommodityGrade
